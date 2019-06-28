7644683d2c5f0e87ec0b053c2d5d51631c2df6b5
kynk94 <kynk94@naver.com>
2019-06-28 09:30:59 +0900

    example data

--- Dance_Face/make_gif.py
+++ Dance_Face/make_gif.py
@@ -3,36 +3,37 @@ import matplotlib.pyplot as plt
 import cv2
 from pathlib import Path
 from skimage import io
-
+import os
 import matplotlib.animation as ani
 from IPython.display import HTML
 import matplotlib
 
-landmark_3D_dir = Path("./data/train/landmark/label")
-landmark_3D_img_paths = sorted(landmark_3D_dir.iterdir())
-# source_dir = Path('./data/source/test_img')
-# target_dir = Path('./results/target/test_latest/images')
-# label_dir = Path('./data/source/test_label_ori')
-#
-# source_img_paths = sorted(source_dir.iterdir())
-# target_synth_paths = sorted(target_dir.glob('*synthesized*'))
-# target_label_paths = sorted(label_dir.iterdir())
+landmark_dir = Path("./data/train/landmark")
 
+want = "label"
 
-def animate(nframe):
+want_dir = landmark_dir.joinpath(want)
+want_img_paths = sorted(want_dir.iterdir())
+
+gif_dir = Path("./gif")
+gif_dir.mkdir(exist_ok=True)
+
+def animate(nframe, img_paths=want_img_paths):
     ax1.clear()
 
-    landmark_3D_img = io.imread(landmark_3D_img_paths[nframe])
-    ax1.imshow(landmark_3D_img)
+    img = io.imread(img_paths[nframe])
+    ax1.imshow(img)
     ax1.set_xticks([])
     ax1.set_yticks([])
 
-fig = plt.figure(figsize=(10, 10))
+fig = plt.figure(figsize=(5, 5))
 ax1 = fig.add_subplot(111)
 
-anim = ani.FuncAnimation(fig, animate, frames=len(landmark_3D_img_paths), interval=1000 / 24)
+anim = ani.FuncAnimation(fig, animate, frames=len(want_img_paths), interval=1000 / 24)
 plt.close()
 
 js_anim = HTML(anim.to_jshtml())
 
-anim.save("label.gif", writer="imagemagick")
+anim.save(gif_dir.joinpath(want+".gif"), writer="imagemagick")
+if "None0000000.png" in os.listdir():
+    os.remove("None0000000.png")
